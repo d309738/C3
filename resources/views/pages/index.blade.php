95c250d12448a6280781ff969940625267435654
Mohamad3212 <d312093@edu.curio.nl>
2025-11-12 11:10:06 +0100

    wat css

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,3 +1,15 @@
 <x-layouts.app>
-    <h1>hello</h1>
+    <h1>Schoolvoetbal</h1>
+    <div class="home">
+        <div>
+            <div class="top5">
+                <h3>Top 5: </h3>
+                <h5>Ajax</h5>
+                <h5>Fynord</h5>
+                <h5>PSV</h5>
+                <h5>Utrecht</h5>
+                <h5>AZ</h5>
+            </div>
+        </div>
+    </div>
 </x-layouts.app>
