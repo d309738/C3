42d9114fedfeff7a0f78c1ea9cd38cbe85f987e6
Mohamad3212 <d312093@edu.curio.nl>
2025-11-12 11:53:33 +0100

    breeze

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::middleware(['auth'])->group(function () {
+    
+})->middleware('verified');
+
 require __DIR__.'/auth.php';
