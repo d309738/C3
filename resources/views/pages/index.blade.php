bb5141f18e1f3873210150a44f136ebdaf70df80
Mohamad3212 <d312093@edu.curio.nl>
2025-11-13 10:04:48 +0100

    logout verbeterd

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,7 @@
 <x-layouts.app>
     <h1>Schoolvoetbal</h1>
-    <div class="home">
-        <div class="justify-between">
+    <div class="flex-column gap-10">
+        <div class="flex justify-between">
             <div class="bg-gray-200 p-4">
                 <h3>Top 5: </h3>
                 <h5>Ajax</h5>
@@ -11,11 +11,16 @@
                 <h5>AZ</h5>
             </div>
             <div>
-                <img src="https://www.ajax.nl/media/2rwemxdz/1819historie.jpg" alt="">
+                <img src="https://www.ajax.nl/media/2rwemxdz/1819historie.jpg" alt="" class="w-[200px]">
             </div>
         </div>
-        <div>
-
+        <div class="flex justify-between">
+            <div>
+                <h3>Wedstrijdschema</h3>
+            </div>
+            <div>
+                <h3>Mijn team</h3>
+            </div>
         </div>
     </div>
 </x-layouts.app>
